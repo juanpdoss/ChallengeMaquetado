7ac11591b857acb9ee99a09f2d004183353a625f
juanpdoss <juanpdoss@gmail.com>
2021-09-23 17:58:36 -0300

    practica material tags

--- challenge/src/app/pages/sign-up/sign-up.component.ts
+++ challenge/src/app/pages/sign-up/sign-up.component.ts
@@ -14,6 +14,7 @@ export class SignUpComponent implements OnInit {
   numberEmployees: string = '';
   typeOfbusiness: string = '';
   placeOfBusinessOperation: string = '';
+  bool: boolean = true;
 
   constructor() {}
 
